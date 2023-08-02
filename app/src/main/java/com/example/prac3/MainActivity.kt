04bd88bc954cb691fcef13d4dfe5bd803f7bc6af
Lim Xin Ying <limxy-wm20@student.tarc.edu.my>
2023-08-02 14:17:00 +0800

    First change

--- app/src/main/java/com/example/prac3/MainActivity.kt
+++ app/src/main/java/com/example/prac3/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.prac3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun btnAdd(view: View) {}
+
 }
